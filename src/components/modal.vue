c3de7c223e15898fec94e1650342aec1df66fe3f
90L9I4D4KC9NTY3\Administrator <767423816@qq.com>
2021-06-06 15:01:41 +0800

    移动项目

---
+++ src/components/modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <teleport to="#modal">
+    <div class="hello">hello</div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "modal",
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<style scoped lang="scss">
+</style>
