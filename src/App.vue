2760aec1337c839fdc1885291ed7424fb7a5be8c
90L9I4D4KC9NTY3\Administrator <767423816@qq.com>
2021-06-06 13:51:52 +0800

    webmovie

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav"></div>
+  <router-view />
+</template>
+
+<style lang="scss">
+body,
+html,
+#app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
